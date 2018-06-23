07a930f1b95b8eb1dfb7932d5f0bb3c9645d2506
Joshua T Corbin <jcorbin@wunjo.org>
2018-06-23 10:12:25 -0700

    Move command running core out of main.go

--- commands.go
+++ commands.go
@@ -5,11 +5,65 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"os"
 	"regexp"
+	"syscall"
 )
 
 type command interface {
 	Process(buf []byte, ateof bool) (off int, err error)
+}
+
+//// running commands
+
+type filelike interface {
+	Name() string
+	Stat() (os.FileInfo, error)
+	Fd() uintptr
+}
+
+func runCommand(cmd command, r io.Reader, useMmap bool) error {
+	if f, canMmap := r.(filelike); useMmap && canMmap {
+		buf, fin, err := mmap(f)
+		if err == nil {
+			defer fin()
+			_, err = cmd.Process(buf, true)
+		}
+		return err
+	}
+
+	if rf, canReadFrom := cmd.(io.ReaderFrom); canReadFrom {
+		_, err := rf.ReadFrom(r)
+		return err
+	}
+
+	// TODO if (some) commands implement io.Writer, then could upgrade to r.(WriterTo)
+
+	rb := readBuf{buf: make([]byte, 0, minRead)} // TODO configurable buffer size
+	return rb.Process(cmd, r)
+}
+
+func mmap(f filelike) ([]byte, func() error, error) {
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, nil, fmt.Errorf("mmap: cannot stat %q: %v", f.Name(), err)
+	}
+
+	size := fi.Size()
+	if size <= 0 {
+		return nil, nil, fmt.Errorf("mmap: file %q has negative size", f.Name())
+	}
+	if size != int64(int(size)) {
+		return nil, nil, fmt.Errorf("mmap: file %q is too large", f.Name())
+	}
+
+	data, err := syscall.Mmap(int(f.Fd()), 0, int(size), syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
+		return nil, nil, err
+	}
+	return data, func() error {
+		return syscall.Munmap(data)
+	}, nil
 }
 
 //// extraction by pattern
--- main.go
+++ main.go
@@ -5,11 +5,9 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
 	"io"
 	"log"
 	"os"
-	"syscall"
 )
 
 func main() {
@@ -35,46 +33,6 @@ func run() error {
 	}
 
 	return withProf(func() error {
-		switch impl := cmd.(type) {
-		case io.ReaderFrom:
-			_, err := impl.ReadFrom(os.Stdin)
-			return err
-		}
-
-		if useMmap {
-			buf, fin, err := mmap(os.Stdin)
-			if err != nil {
-				return err
-			}
-			defer fin()
-			_, err = cmd.Process(buf, true)
-			return err
-		}
-
-		rb := readBuf{buf: make([]byte, 0, minRead)} // TODO configurable buffer size
-		return rb.Process(cmd, os.Stdin)
+		return runCommand(cmd, os.Stdin, useMmap)
 	})
-}
-
-func mmap(f *os.File) ([]byte, func() error, error) {
-	fi, err := f.Stat()
-	if err != nil {
-		return nil, nil, fmt.Errorf("mmap: cannot stat %q: %v", f.Name(), err)
-	}
-
-	size := fi.Size()
-	if size <= 0 {
-		return nil, nil, fmt.Errorf("mmap: file %q has negative size", f.Name())
-	}
-	if size != int64(int(size)) {
-		return nil, nil, fmt.Errorf("mmap: file %q is too large", f.Name())
-	}
-
-	data, err := syscall.Mmap(int(f.Fd()), 0, int(size), syscall.PROT_READ, syscall.MAP_SHARED)
-	if err != nil {
-		return nil, nil, err
-	}
-	return data, func() error {
-		return syscall.Munmap(data)
-	}, nil
 }
