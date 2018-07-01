0199a1063b00c955de02915007d71cee260ce0d8
Joshua T Corbin <jcorbin@wunjo.org>
2018-06-30 20:27:55 -0400

    Add basic documentation for processor(IO)

--- command.go
+++ command.go
@@ -109,10 +109,19 @@ type command interface {
 	Create(command, environment) (processor, error)
 }
 
+// processor represents a piece of structure processing logic. Process gets
+// called for each piece of matched sub-structure within some level of
+// structure. The last flag indicates whether this is the last piece of
+// sub-structure. After Process has been called with last=true, it may be
+// called again to start processing the next (semantically sibling) structure
+// to the one just ended.
 type processor interface {
 	Process(buf []byte, last bool) error
 }
 
+// processorIO is a processor that supports streaming. Such a processor may be
+// used as a top level processor starting out a command chain; all other
+// processors must be wrapped/driven by a processorIO.
 type processorIO interface {
 	processor
 	io.ReaderFrom
