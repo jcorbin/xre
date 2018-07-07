800d0042a1285b49fd3962862a8fae5dc5dd622a
Joshua T Corbin <jcorbin@wunjo.org>
2018-07-06 22:16:16 -0700

    Factor out scanBalanced

--- balance.go
+++ balance.go
@@ -18,50 +18,51 @@ type extractBalanced struct {
 }
 
 func (bb betweenBalanced) Process(buf []byte, last bool) error {
+	for {
+		if len(buf) == 0 {
+			return nil
+		}
+		if loc, found := scanBalanced(bb.open, bb.close, buf); found {
+			if err := bb.next.Process(buf[loc[0]+1:loc[1]-1], false); err != nil {
+				return err
+			}
+			buf = buf[loc[1]:]
+		}
+	}
+}
+
+func (eb extractBalanced) Process(buf []byte, last bool) error {
+	for {
+		if len(buf) == 0 {
+			return nil
+		}
+		if loc, found := scanBalanced(eb.open, eb.close, buf); found {
+			if err := eb.next.Process(buf[loc[0]:loc[1]], false); err != nil {
+				return err
+			}
+			buf = buf[loc[1]:]
+		}
+	}
+}
+
+func scanBalanced(open, close byte, buf []byte) ([2]int, bool) {
 	// TODO escaping? quoting?
 	level, start := 0, 0
 	for off := 0; off < len(buf); off++ {
 		switch buf[off] {
-		case bb.open:
-			if level == 0 {
-				start = off + 1
-			}
-			level++
-		case bb.close:
-			level--
-			if level < 0 {
-				level = 0
-			} else if level == 0 {
-				m := buf[start:off] // extracted match
-				if err := bb.next.Process(m, false); err != nil {
-					return err
-				}
-			}
-		}
-	}
-	return nil
-}
-
-func (eb extractBalanced) Process(buf []byte, last bool) error {
-	// TODO escaping? quoting?
-	for level, start, off := 0, 0, 0; off < len(buf); off++ {
-		switch buf[off] {
-		case eb.open:
+		case open:
 			if level == 0 {
 				start = off
 			}
 			level++
-		case eb.close:
+		case close:
 			level--
 			if level < 0 {
 				level = 0
 			} else if level == 0 {
-				m := buf[start : off+1] // extracted match
-				if err := eb.next.Process(m, false); err != nil {
-					return err
-				}
+				return [2]int{start, off + 1}, true
 			}
 		}
 	}
-	return nil
+	return [2]int{0, 0}, false
 }
