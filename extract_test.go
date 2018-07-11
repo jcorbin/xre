3842dccd16a72afa71ad98d511623e993272d6fe
Joshua T Corbin <jcorbin@wunjo.org>
2018-07-11 08:06:24 -0700

    Add a basic extract test

---
+++ extract_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+func Test_extract(t *testing.T) {
+	cmdTestCases{
+		{
+			name: "line extraction",
+			cmd:  `x/.*\n/ p%"%q\n"`,
+			in: stripBlockSpace(`
+			aee
+			bee	dee
+			cee
+			`),
+			out: stripBlockSpace(`
+			"aee\n"
+			"bee\tdee\n"
+			"cee\n"
+			`),
+		},
+
+		{
+			name: "line extraction (submatch)",
+			cmd:  `x/(.*)\n/ p%"%q\n"`,
+			in: stripBlockSpace(`
+			aee
+			bee	dee
+			cee
+			`),
+			out: stripBlockSpace(`
+			"aee"
+			"bee\tdee"
+			"cee"
+			`),
+		},
+
+		{
+			name: "field extraction",
+			cmd:  `x/(.*)\n/ x/^([^\s]+).*$/ p%"%q\n"`,
+			in: stripBlockSpace(`
+			aee
+			bee	dee
+			cee
+			`),
+			out: stripBlockSpace(`
+			"aee"
+			"bee"
+			"cee"
+			`),
+		},
+	}.run(t)
+}
