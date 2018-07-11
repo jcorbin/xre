da8d6e8f8acaff183f68a2c254db4a614c8c1029
Joshua T Corbin <jcorbin@wunjo.org>
2018-07-11 08:39:53 -0700

    Add basic filter test

---
+++ filter_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+var catAdjacentThings = stripBlockSpace(`
+bird
+cat
+dog
+bobcat
+fox
+cantaloupe
+grumpy cat
+book
+catalog
+cab
+truck
+car
+`)
+
+func Test_filter(t *testing.T) {
+	// TODO leverage line-oriented defaults once available
+
+	cmdTestCases{
+		{
+			name: "finding cats",
+			cmd:  `y/\n/ g/cat/ p"\n"`,
+			in:   catAdjacentThings,
+			out: stripBlockSpace(`
+			cat
+			bobcat
+			grumpy cat
+			catalog
+			`),
+		},
+
+		{
+			name: "excising cats",
+			cmd:  `y/\n/ v/cat/ p"\n"`,
+			in:   catAdjacentThings,
+			out: stripBlockSpace(`
+			bird
+			dog
+			fox
+			cantaloupe
+			book
+			cab
+			truck
+			car
+			`),
+		},
+	}.run(t)
+}
