46b18b503f6ee4784e0d5e56c1b25fa72721d7cf
Joshua T Corbin <jcorbin@wunjo.org>
2018-07-13 14:32:04 -0700

    TODO *Predicates

--- predicate.go
+++ predicate.go
@@ -6,6 +6,10 @@ type predicate interface {
 	test([]byte) bool
 }
 
+type anyPredicate []predicate
+type allPredicate []predicate
+type notPredicate struct{ predicate }
+
 type predicateProcessor struct {
 	predicate
 	next Processor
@@ -24,6 +28,26 @@ func (pp predicateProcessor) Process(buf []byte, last bool) error {
 		return pp.next.Process(nil, true)
 	}
 	return nil
+}
+
+func (np notPredicate) test(buf []byte) bool { return !np.predicate.test(buf) }
+
+func (ap anyPredicate) test(buf []byte) bool {
+	for _, p := range ap {
+		if p.test(buf) {
+			return true
+		}
+	}
+	return false
+}
+
+func (ap allPredicate) test(buf []byte) bool {
+	for _, p := range ap {
+		if !p.test(buf) {
+			return false
+		}
+	}
+	return true
 }
 
 // TODO apply predicates to groups
